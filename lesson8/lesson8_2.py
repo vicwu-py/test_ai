4109a6f5548cd481c65e6f60df3ba691a33f9704
vicwu-py <wzong56@gmail.com>
2026-01-28 13:08:11 +0000

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -2,7 +2,7 @@ import os
 
 #使用names.txt的絕對路徑
 current_dir = os.path.dirname(os.path.abspath("__file__"))
-file_path = os.path.join(current_dir,'lesson8','assets','names.txt')
+file_path = os.path.join(current_dir,'assets','names.txt')
 
 with open(file_path,encoding="utf-8")as file:
     content = file.read()
