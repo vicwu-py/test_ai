0527a4a5b85ba6d92df6ea401e5d27664f940a70
vicwu-py <wzong56@gmail.com>
2026-02-04 12:11:03 +0000

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -1,11 +1,14 @@
 import os
 
-#使用names.txt的絕對路徑
-current_dir = os.path.dirname(os.path.abspath("__file__"))
-file_path = os.path.join(current_dir,'assets','names.txt')
+def get_names(file_name):
+    #使用names.txt的絕對路徑
+    current_dir=os.path.dirname(os.path.abspath("_file_"))
+    file_path=os.path.join(current_dir,'assets',file_name)
 
-with open(file_path,encoding="utf-8")as file:
-    content = file.read()
+    with open(file_path,encoding='utf-8')as file:
+        content=file.read()
 
-names=content.split('.')
+    return content.split(',')
+    
+names=get_names("names.txt")
 print(names)
